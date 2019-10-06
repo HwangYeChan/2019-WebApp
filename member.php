fa41554abc3d8857e061df9a751ae2e08a62667e
Hee-Jae <jhj967878@naver.com>
2019-10-06 19:04:52 +0900

    upload memeber.php 10/06

--- member.php
+++ member.php
@@ -8,10 +8,33 @@
         <?php include('assets/php/navbar.php'); ?>
         <!-- Article -->
         <div class="article">
-            <h1><span class="h1-border">이건 제목이야</span></h1>
-            <p>여기는 본문이야 Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam nisi ipsum necessitatibus autem, cupiditate esse iusto accusamus corrupti minima odio natus expedita voluptatum unde enim magnam illum quia quam dolorum?</p>
-            <h2>이건 제목이야2</h2>
-            <p>여기는 본문이야 Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam nisi ipsum necessitatibus autem, cupiditate esse iusto accusamus corrupti minima odio natus expedita voluptatum unde enim magnam illum quia quam dolorum?</p>
+            <h1><span class="h1-border">Members</span></h1>
+            <div class="position">
+                <h2> Professor </h2>
+            <div class="human">
+                <img clas="photo" src="#">
+                <div id="info">
+                    <ul>
+                        <li class="name"> Scott Uk-Jin Lee </li>
+                        <li> scottlee@hanyang.ac.kr </li>
+                        <li><a href="TBA" target="_blank"> TBA</a></li>
+                        <li>an asst. prof. in the CSE Dept. @ HYU (ERICA)<br />
+                            leading this Laboratory!</li>
+						</ul>
+				</div>
+				<div id="interest">
+					<ul>
+                        <li>Career</li>
+                        <li>11 -      : Assistant Professor in CSE Dept. @ Hanyang University, Korea<br />
+                            09 - 11 : Postdoc @ CEA LIST, France <br />
+                            05 - 06 : SE Programmer in SE Dept. @ University of Auckland, NZ <br />
+                            04 - 04 : Visiting Researcher @ National University of Singapore<br />
+                            04 - 08 : Ph.D in Computer Science @ University of Auckland, NZ<br />
+                            00 - 03 : BE in Software Engineering @ University of Auckland, NZ </li>
+                    </ul>
+                </div> <!-- interest -->
+			</div> <!-- human -->
+		    </div> <!-- position -->
         </div>
         <!-- Footer -->
         <?php include('assets/php/footer.php'); ?>
