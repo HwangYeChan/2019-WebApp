1349f322fa9aa422abd54e30db396f03021a39ef
hyowonlee <hyowonlee@hyowonleeui-MacBook-Pro.local>
2019-10-09 21:22:40 +0900

    notices : delete simbol

--- notice.php
+++ notice.php
@@ -1,82 +1,117 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
+
 <html lang="ko">
-  <?php include('assets/php/head.php'); ?>
-
-  <!-- FontAwesome icons -->
-  <link rel="stylesheet" href="https://use.fontawesome.com/74dfc6cf47.css">
-  <!-- Core BootStrap CSS -->
-  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"> -->
-  <!-- Custom CSS -->        
-  <!-- <link rel="stylesheet" href="/static/css/thickbox.css"> -->
-  <link rel="stylesheet" href="/static/css/main.css?test=1">
-
-  <body>
-    <!-- Navbar -->
-    <?php include('assets/php/navbar.php'); ?>
-
-    <div class="article">
-          <div class="container main outer">
-          <div class="well">
-          <div class="list-group">
+    <?php include('./assets/php/head.php'); ?>
+    <?php $currentpage = 'NOTICE'; ?>
+    <body>
+        <!-- Navbar -->
+        <?php include('assets/php/navbar.php'); ?>
+        <!-- Article -->
+        <div class="article">
+            <h1><span class="h1-border">Notices</span></h1>
             
-            <div class="list-group-item">
-              <div class="row-action-primary">
-                <i class="fa fa-sticky-note"></i>
-              </div>
-              <div class="row-content">
-                <div class="least-content">04 Sep 2019</div>
-                <h4 class="list-group-item-heading"><a href="/others/2019/09/04/visit.html">testest</a></h4>
-
-                <p class="list-group-item-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam nisi ipsum necessitatibus autem, cupiditate esse iusto...</p>
-              </div>
-            </div>
-            <div class="list-group-separator"></div>
-            
-            <div class="list-group-item">
-              <div class="row-action-primary">
-                <i class="fa fa-sticky-note"></i>
-              </div>
-              <div class="row-content">
-                <div class="least-content">01 Sep 2019</div>
-                <h4 class="list-group-item-heading"><a href="/others/2019/09/01/prof.html">D B D B dip</a></h4>
-
-                <p class="list-group-item-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam nisi ipsum necessitatibus autem, cupiditate esse iusto...</p>
-              </div>
-            </div>
-            <div class="list-group-separator"></div>
-            
-            <div class="list-group-item">
-              <div class="row-action-primary">
-                <i class="fa fa-sticky-note"></i>
-              </div>
-              <div class="row-content">
-                <div class="least-content">15 Aug 2019</div>
-                <h4 class="list-group-item-heading"><a href="/others/2019/08/15/postphd.html">Scottlove</a></h4>
-
-                <p class="list-group-item-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam nisi ipsum necessitatibus autem, cupiditate esse iusto...</p>
-              </div>
-            </div>
-            <div class="list-group-separator"></div>
-            
-            <div class="list-group-item">
-              <div class="row-action-primary">
-                <i class="fa fa-sticky-note"></i>
-              </div>
-              <div class="row-content">
-                <div class="least-content">01 Aug 2019</div>
-                <h4 class="list-group-item-heading"><a href="/others/2019/08/01/phds.html">jo e reum jeong hae ya hae</a></h4>
-
-                <p class="list-group-item-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam nisi ipsum necessitatibus autem, cupiditate esse iusto...</p>
-              </div>
+            <div class="notices-section">
+                <div class="post">
+                    <div class="post-simbol">
+                        <!-- <img src="images/simbol.png" width=40% height=40%> -->
+                    </div>
+                    <div class="post_content">
+                        <div class="post-title">
+                            <a href="#">연구원 모집</a>
+                        </div>
+                        <div class="post-date">
+                            29 Aug 2014
+                        </div>
+                        <div class="post-summary">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam nisi ipsum necessitatibus autem, cupiditate esse iusto accusamus corrupti minim...
+                        </div>
+                    </div>
+                    <hr/>
+                </div>
             </div>
 
-          </div>
+            <div class="notices-section">
+                <div class="post">
+                    <div class="post-title">
+                        <a href="#">2014 PL-SE First Joint Workshop</a>
+                    </div>
+                    <div class="post-date">
+                        29 Apr 2014
+                    </div>
+                    <div class="post-summary">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam nisi ipsum necessitatibus autem, cupiditate esse iusto accusamus corrupti minim...
+                    </div>
+                    <hr/>
+                </div>
+            </div>
+
+            <div class="notices-section">
+                <div class="post">
+                    <div class="post-title">
+                        <a href="#">Welcome to SELab Homepage!</a>
+                    </div>
+                    <div class="post-date">
+                        06 Mar 2014
+                    </div>
+                    <div class="post-summary">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam nisi ipsum necessitatibus autem, cupiditate esse iusto accusamus corrupti minim...
+                    </div>
+                    <hr/>
+                </div>
+            </div>
+
+            <div class="notices-section">
+                <div class="post">
+                    <div class="post-simbol">
+                        <!-- <img src="images/simbol.png" width=40% height=40%> -->
+                    </div>
+                    <div class="post_content">
+                        <div class="post-title">
+                            <a href="#">연구원 모집</a>
+                        </div>
+                        <div class="post-date">
+                            29 Aug 2014
+                        </div>
+                        <div class="post-summary">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam nisi ipsum necessitatibus autem, cupiditate esse iusto accusamus corrupti minim...
+                        </div>
+                    </div>
+                    <hr/>
+                </div>
+            </div>
+
+            <div class="notices-section">
+                <div class="post">
+                    <div class="post-title">
+                        <a href="#">2014 PL-SE First Joint Workshop</a>
+                    </div>
+                    <div class="post-date">
+                        29 Apr 2014
+                    </div>
+                    <div class="post-summary">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam nisi ipsum necessitatibus autem, cupiditate esse iusto accusamus corrupti minim...
+                    </div>
+                    <hr/>
+                </div>
+            </div>
+
+            <div class="notices-section">
+                <div class="post">
+                    <div class="post-title">
+                        <a href="#">Welcome to SELab Homepage!</a>
+                    </div>
+                    <div class="post-date">
+                        06 Mar 2014
+                    </div>
+                    <div class="post-summary">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam nisi ipsum necessitatibus autem, cupiditate esse iusto accusamus corrupti minim...
+                    </div>
+                    <hr/>
+                </div>
+            </div>
+
         </div>
-      </div>
-    </div>
-
-
-    <!-- Footer -->
-    <?php include('assets/php/footer.php'); ?>
-  </body>
+        <!-- Footer -->
+        <?php include('assets/php/footer.php'); ?>
+    </body>
 </html>
