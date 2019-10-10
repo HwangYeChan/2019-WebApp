09b3d4b7e7142f67dc389df033950ee1838f4144
hyowonlee <hyowonlee@hyowonleeui-MacBook-Pro.local>
2019-10-10 13:26:22 +0900

    Courses

--- courses.php
+++ courses.php
@@ -8,10 +8,52 @@
         <?php include('assets/php/navbar.php'); ?>
         <!-- Article -->
         <div class="article">
-            <h1><span class="h1-border">이건 제목이야</span></h1>
-            <p>여기는 본문이야 Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam nisi ipsum necessitatibus autem, cupiditate esse iusto accusamus corrupti minima odio natus expedita voluptatum unde enim magnam illum quia quam dolorum?</p>
-            <h2>이건 제목이야2</h2>
-            <p>여기는 본문이야 Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam nisi ipsum necessitatibus autem, cupiditate esse iusto accusamus corrupti minima odio natus expedita voluptatum unde enim magnam illum quia quam dolorum?</p>
+            <h1><span class="h1-border">Courses</span></h1>
+
+            <div class="courses-section">
+                <div class="courses-title">Undergraduate Courses</div>
+
+                <div class="courses-content">
+                    <div class="courses">
+                        <div class="courses-code">CSE326</div>
+                        <div class="courses-name">Web Application Development</div>
+                        <div class="courses-home">Home</div>
+                        <div class="courses-slides">Slides</div>
+                    </div>
+                </div>
+
+                <div class="courses-content">
+                    <div class="courses">
+                        <div class="courses-code">CSE406</div>
+                        <div class="courses-name">Software Engineering</div>
+                        <div class="courses-home">Home</div>
+                        <div class="courses-slides">Slides</div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="courses-section">
+                <div class="courses-title">Postgraduate Courses</div>
+
+                <div class="courses-content">
+                    <div class="courses">
+                        <div class="courses-code">CSE609</div>
+                        <div class="courses-name">Model Checking</div>
+                        <div class="courses-home">Home</div>
+                        <div class="courses-slides">Slides</div>
+                    </div>
+                </div>
+
+                <div class="courses-content">
+                    <div class="courses">
+                        <div class="courses-code">CSE639</div>
+                        <div class="courses-name">Special Topics in Software Engineering</div>
+                        <div class="courses-home">Home</div>
+                        <div class="courses-slides">Slides</div>
+                    </div>
+                </div>
+            </div>
         </div>
         <!-- Footer -->
         <?php include('assets/php/footer.php'); ?>
