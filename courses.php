c8083dccdee4e09a55e9738c61d0df3f6bd9f4c1
hyowonlee <hyowonlee@hyowonleeui-MacBook-Pro.local>
2019-10-10 14:41:41 +0900

    Courses

--- courses.php
+++ courses.php
@@ -11,14 +11,14 @@
             <h1><span class="h1-border">Courses</span></h1>
 
             <div class="courses-section">
-                <div class="courses-title">Undergraduate Courses</div>
+                <div class="courses-title">Undergraduate Courses<br/></div>
 
                 <div class="courses-content">
                     <div class="courses">
                         <div class="courses-code">CSE326</div>
                         <div class="courses-name">Web Application Development</div>
-                        <div class="courses-home">Home</div>
-                        <div class="courses-slides">Slides</div>
+                        <div class="courses-slides"><a href="#">Slides</a></div>
+                        <div class="courses-home"><a href="#">Home</a></div>
                     </div>
                 </div>
 
@@ -26,8 +26,8 @@
                     <div class="courses">
                         <div class="courses-code">CSE406</div>
                         <div class="courses-name">Software Engineering</div>
-                        <div class="courses-home">Home</div>
-                        <div class="courses-slides">Slides</div>
+                        <div class="courses-slides"><a href="#">Slides</a></div>
+                        <div class="courses-home"><a href="#">Home</a></div>
                     </div>
                 </div>
             </div>
@@ -40,8 +40,8 @@
                     <div class="courses">
                         <div class="courses-code">CSE609</div>
                         <div class="courses-name">Model Checking</div>
-                        <div class="courses-home">Home</div>
-                        <div class="courses-slides">Slides</div>
+                        <div class="courses-slides"><a href="#">Slides</a></div>
+                        <div class="courses-home"><a href="#">Home</a></div>
                     </div>
                 </div>
 
@@ -49,8 +49,8 @@
                     <div class="courses">
                         <div class="courses-code">CSE639</div>
                         <div class="courses-name">Special Topics in Software Engineering</div>
-                        <div class="courses-home">Home</div>
-                        <div class="courses-slides">Slides</div>
+                        <div class="courses-slides"><a href="#">Slides</a></div>
+                        <div class="courses-home"><a href="#">Home</a></div>
                     </div>
                 </div>
             </div>
