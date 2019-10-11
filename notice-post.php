465b96c9579059bdc9d774b8d87187d8cea729d2
hyowonlee <hyowonlee@hyowonleeui-MacBook-Pro.local>
2019-10-11 14:03:29 +0900

    notice-post

--- notice-post.php
+++ notice-post.php
@@ -9,6 +9,17 @@
         <!-- Article -->
         <div class="article">
             <h1><span class="h1-border">연구원 모집</span></h1>
+            <div class="notice-post-info">
+                <div id="notice-post-writer">작성자 : Scott Uk-Jin Lee</div>
+                <div id="notice-post-date">작성일 : 11 Oct 2019</div>
+                <div id="notice-post-hits">조회수 : 54회</div>
+                <hr/>
+            </div>
+            <div class="notice-post-file-list">
+                <div class="notice-post-file"><span>첨부파일</span> : <a href="">asdf.png</a></div>
+                <div class="notice-post-file"><span>첨부파일</span> : <a href="">zxcv.docx</a></div>
+                <div class="notice-post-file"><span>첨부파일</span> : <a href="">qwere.jpg</a></div>
+            </div>
             <div class="notice-post-content">
                 여기는 본문이야 Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam nisi ipsum necessitatibus autem, cupiditate esse iusto accusamus corrupti minima odio natus expedita voluptatum unde enim magnam illum quia quam dolorum?여기는 본문이야 Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam nisi ipsum necessitatibus autem, cupiditate esse iusto accusamus corrupti minima odio natus ex<br/><br/>pedita voluptatum unde enim magnam illum quia quam dolorum?여기는 본문이야 Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam nisi ipsum necessitatibus autem, cupiditate esse iusto accusamus corrupti minima odio natus expedita voluptatum unde enim magnam illum quia quam dolorum?여기는 본문이야 Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam nisi ipsum necessitatibus autem, cupiditate esse iusto accusamus corrupti minima odio natus expedita voluptatum unde enim magnam illum quia quam dolorum?
             </div>
